892955db8a7a501763a40297bcc4d8a41a164a4a
Ramyashree034 <151832023+Ramyashree034@users.noreply.github.com>
2025-10-14 21:24:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,8 @@ app.post("/register", async (req, res) => {
     if (
       event &&
       (event.toLowerCase().includes("hackathon") ||
-        event.toLowerCase().includes("byte battle tech"))
+        event.toLowerCase().includes("byte battle tech") ||
+        event.toLowerCase().includes("debugging tech"))
     ) {
       return res.status(400).json({
         message: `🚫 Registrations for ${event} are now closed.`,
